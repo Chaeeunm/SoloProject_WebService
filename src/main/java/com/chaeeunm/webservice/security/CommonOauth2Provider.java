20c10cbbede6e3ee64fa2141802c59538f5fea1e
Chaeeunm <rlacodms0525@naver.com>
2023-05-21 23:54:21 +0900

    코드정리

--- src/main/java/com/chaeeunm/webservice/security/CommonOauth2Provider.java
+++ src/main/java/com/chaeeunm/webservice/security/CommonOauth2Provider.java
@@ -4,16 +4,7 @@ package com.chaeeunm.webservice.security;
 import java.util.Locale;
 
 public enum CommonOauth2Provider {
-    GOOGLE {
-        public ClientRegistration.Builder getBuilder(String registrationId) {
-            ClientRegistration.Builder builder = getBuilder(registrationId, ClientAuthenticationMethod.POST, DEFAULT_LOGIN_REDIRECT_URL);
-            builder.scope(new String[]{"profile", "email"});
-            builder.authorizationUri("https://accounts.google.com/o/oauth2/v2/auth");
-            builder.tokenUri("https://www.googleapis.com/oauth2/v4/token");
-            builder.userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo");
-            builder.userNameAttributeName("sub");
-            builder.clientName("Google");
-            return builder;
+
         }
     };
 }
